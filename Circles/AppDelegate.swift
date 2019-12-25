18dc566d577ebcc5c79e75a652496aef14489fab
Venky Venkatakrishnan <eeshwar1@gmail.com>
2019-12-25 17:27:04 -0600

    Initial Commit

---
+++ Circles/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Circles
+//
+//  Created by Venky Venkatakrishnan on 12/25/19.
+//  Copyright © 2019 Venky UL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
